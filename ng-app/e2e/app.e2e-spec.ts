73e786d4f0820570b273a020b30017a5884e71a6
dmitrynovik <cryinstone@gmail.com>
2017-06-25 18:25:42 +1000

    fixed e2e

--- ng-app/e2e/app.e2e-spec.ts
+++ ng-app/e2e/app.e2e-spec.ts
@@ -3,9 +3,9 @@
 describe("Web numeric sequences E2E tests", () => {
 
     function checkSequence(id: string, values: string) {
-        it("checking " + id, () => {
+        it("checking sequence: " + id, () => {
             const vals = element(by.id(id)).element(by.css(".sequence-values")).getText();
-            expect(vals).toEqual(values);
+            expect<any>(vals).toEqual(values);
         });
     }
 
@@ -18,21 +18,22 @@ describe("Web numeric sequences E2E tests", () => {
         const apiWaitTimeout = 5000;
         element(by.id("submit")).click();
         browser.sleep(apiWaitTimeout);
+        console.log("Setup done.");
     });
 
     const title = "Numeric sequences generator.";
     it("title must display: " + title, () => {
-        expect(element(by.tagName("h1")).getText()).toEqual(title);
+        expect<any>(element(by.tagName("h1")).getText()).toEqual(title);
     });
 
     const help = "Generates some fancy numeric sequences";
     it("help must display: " + help, () => {
-        expect(element(by.css(".help")).getText()).toEqual(help);
+        expect(<any>element(by.css(".help")).getText()).toEqual(help);
     });
 
     const label = "Please enter the limit";
     it("label must display: " + label, () => {
-        expect(element(by.tagName("label")).getText()).toEqual(label);
+        expect<any>(element(by.tagName("label")).getText()).toEqual(label);
     });
 
     checkSequence("FibonacciNumbers", "1,1,2,3,5,8");
@@ -40,4 +41,10 @@ describe("Web numeric sequences E2E tests", () => {
     checkSequence("OddPositiveNaturalNumbers", "1,3,5,7,9");
     checkSequence("PositiveNaturalNumbers", "1,2,3,4,5,6,7,8,9,10");
     checkSequence("WeirdNaturalNumbers", "1,2,C,4,E,C,7,8,C,E");
+
+    it("must enforce positive numbers in input", () => {
+        const input = element(by.id("limit"));
+        input.sendKeys("-1");
+        expect(input.getAttribute("class")).toContain("ng-invalid");
+    });
 });
